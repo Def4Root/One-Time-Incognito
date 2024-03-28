1790fa23bda97df11919acba6466ce46dbf26660
def4root <def4root@proton.me>
2024-03-28 18:49:42 +0900

    Fix: console messages for service worker

--- background.js
+++ background.js
@@ -1,27 +1,8 @@
 /*
 * One Time Incognito
-* Usage is written in `/README.md` file.
-~
-~ MIT License
-~ Copyright © 2024 Def4Root
-~
-~ Permission is hereby granted, free of charge, to any person obtaining a copy
-~ of this software and associated documentation files (the "Software"), to deal
-~ in the Software without restriction, including without limitation the rights
-~ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-~ copies of the Software, and to permit persons to whom the Software is
-~ furnished to do so, subject to the following conditions:
-~ 
-~ The above copyright notice and this permission notice shall be included in all
-~ copies or substantial portions of the Software.
-~ 
-~ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-~ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-~ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-~ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-~ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-~ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-~ SOFTWARE.
+* 
+* Usage: `/README.md`
+* License: `/LICENSE`
 */
 
 let is_enable_incognito_mode = false
@@ -42,6 +23,10 @@ chrome.action.onClicked.addListener(() => toggle_mode())
 
 const toggle_mode = () => {
 	is_enable_incognito_mode = !is_enable_incognito_mode
+
+	console.clear()
+	console.log(is_enable_incognito_mode ? "Incognito mode is enabled" : "Incognito mode is disabled")
+
 	const icon_path = is_enable_incognito_mode
 		? '48-incognito-icon.png'
 		: '48-normal-icon.png'
