544c8678745e1ca5df9f1a564053c7b73a65f770
def4root <def4root@proton.me>
2024-03-29 23:56:16 +0900

    Add shortcut command and improve codes.

--- background.js
+++ background.js
@@ -1,66 +1,49 @@
 /*
-* One Time Incognito
-* 
-* Usage: `/README.md`
-* License: `/LICENSE`
-*/
+ * One Time Incognito
+ *
+ * Usage: `/README.md`
+ * License: `/LICENSE`
+ */
 
-let is_enable_incognito_mode = false
-let start_time,
-	end_time = 0
+//? For shortcut commands
+
+let is_incognito_mode = false
 
 chrome.commands.onCommand.addListener((command) => {
 	switch (command) {
+		case 'toggle_mode':
+			is_incognito_mode = !is_incognito_mode
+			set_mode(is_incognito_mode)
+			break
 		case 'clear_all_history':
-			remoeve_all_history()
+			remove_all_history()
 			break
 		default:
 			break
 	}
 })
 
-chrome.action.onClicked.addListener(() => toggle_mode())
-
-const toggle_mode = () => {
-	is_enable_incognito_mode = !is_enable_incognito_mode
-
-	console.clear()
-	console.log(is_enable_incognito_mode ? "Incognito mode is enabled" : "Incognito mode is disabled")
-
-	const icon_path = is_enable_incognito_mode
-		? '48-incognito-icon.png'
-		: '48-normal-icon.png'
+const set_mode = (is_incognito_mode_ref) => {
+	const icon_path = is_incognito_mode_ref ? '48-incognito-icon.png' : '48-normal-icon.png'
 	chrome.action.setIcon({ path: `./images/icons/${icon_path}` })
 
-	if (is_enable_incognito_mode) {
+	if (is_incognito_mode_ref) {
 		chrome.history.onVisited.addListener(remove_current_history)
 	} else {
 		chrome.history.onVisited.removeListener(remove_current_history)
 	}
 }
 
-const remove_current_history = (result) => {
-	chrome.history.deleteUrl({ url: result.url })
-
-	const current_history_domain = new URL(result.url).hostname
-	const current_history_article = new URL(result.url).pathname
-		.replace(/\/+$/, '')
-		.split('/')
-		.pop()
-
-	if (current_history_article == '') {
-		console.log(`Removed URL ${current_history_domain}`)
-	} else {
-		console.log(
-			`Removed URL ${current_history_domain}/.../${decodeURI(
-				current_history_article
-			)}`
-		)
-	}
+const remove_current_history = (history_item) => {
+	console.log(history_item)
+	chrome.history.deleteUrl({ url: history_item.url })
 }
 
-const remoeve_all_history = () => {
+// Default shortcut: Alt+H
+const remove_all_history = () => {
 	chrome.history.deleteAll(() => {
 		console.log('Removed all history.')
 	})
 }
+
+//? For popup screen
